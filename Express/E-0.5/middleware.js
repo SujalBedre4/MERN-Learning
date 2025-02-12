ee903b47bef5227146d741a6501f2811f613f1f0
SujalBedre4 <sujalbedre4@gmail.com>
2025-02-13 03:22:31 +0530

    Removed from the main file

--- Express/E-0.5/middleware.js
+++
@@ -1,9 +0,0 @@
-// This is our MiddleWare Condition
-module.exports = reqFilter = (req, resp, next) => {
-    // Now, we are applying some conditions:
-    if (!req.query.age) {
-        resp.send('Please provide age');
-    } else {
-        next();
-    }
-}
